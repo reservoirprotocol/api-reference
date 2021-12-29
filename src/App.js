2068b5375a17b06db0793d8d47979099ffcd36a9
Peter Watts <pwattsmail@gmail.com>
2021-12-29 11:09:11 +1100

    dynamic spec

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { API } from '@stoplight/elements';
 import '@stoplight/elements/styles.min.css';
 import './App.css';
 
+let apiSpec = 'https://indexer-v3-api-production.up.railway.app/swagger.json'
+if(process.env.CHAIN_ID==4) {
+  apiSpec = 'https://indexer-v3-api-development.up.railway.app/swagger.json'
+}
+
 function App() {
   return (
     <div className="App">
-      <API apiDescriptionUrl="https://indexer-v3-api-development.up.railway.app/swagger.json" hideInternal="true" />
+      <API apiDescriptionUrl={apiSpec} hideInternal="true" />
     </div>
   );
 }
